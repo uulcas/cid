8ca5082cb0a98f5880c324332833b99c8eafa23a
alecadair <alecadair1@gmail.com>
2023-02-13 18:07:48 +0200

    added placeholders for all files

---
+++ tutorial/common_source_amp.py
@@ -0,0 +1,13 @@
+#
+# Author: Alec Stefan Adair
+#
+# Template and tutorial for common source amp design
+#
+
+import CID as cid
+
+# gainbandwidth specification of 5Ghz
+gbw = 5e9
+
+# capacitive load of 10 fF
+cload = 10e-15
